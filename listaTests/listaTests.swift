fcc016ec5be9c2951e2aa7984540a3ebeb9677b9
Alaksion <lbeurmann.dev@gmail.com>
2026-01-14 12:05:21 -0300

    initial commit

---
+++ listaTests/listaTests.swift
@@ -0,0 +1,17 @@
+//
+//  listaTests.swift
+//  listaTests
+//
+//  Created by Lucca Beurmann on 14/01/26.
+//
+
+import Testing
+@testable import lista
+
+struct listaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
