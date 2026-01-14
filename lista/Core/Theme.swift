9086d0634c32e080731d807aaa2534bb69eb2c44
Alaksion <lbeurmann.dev@gmail.com>
2026-01-14 12:38:41 -0300

    feat: add color mapping object for easy access

---
+++ lista/Core/Theme.swift
@@ -0,0 +1,41 @@
+//
+//  Theme.swift
+//  lista
+//
+//  Created by Lucca Beurmann on 14/01/26.
+//
+
+import Foundation
+import SwiftUI
+
+
+// MARK: - Colors Tokens
+struct AppColors {
+  static let accentForeground      = Color("AccentForeground")
+  static let accent                = Color("Accent")
+  static let accentColor           = Color("AccentColor")
+  static let amber                 = Color("Amber")
+  static let appIcon               = Color("AppIcon")
+  static let background            = Color("Background")
+  static let blue                  = Color("Blue")
+  static let border                = Color("Border")
+  static let card                  = Color("Card")
+  static let cardForeground        = Color("CardForeground")
+  static let cyan                  = Color("Cyan")
+  static let destructive           = Color("Destructive")
+  static let foreground            = Color("Foreground")
+  static let green                 = Color("Green")
+  static let input                 = Color("Input")
+  static let muted                 = Color("Muted")
+  static let mutedForeground       = Color("MutedForeground")
+  static let orange                = Color("Orange")
+  static let pink                  = Color("Pink")
+  static let popover               = Color("Popover")
+  static let popoverForeground     = Color("PopoverForeground")
+  static let primary               = Color("Primary")
+  static let primaryForeground     = Color("PrimaryForeground")
+  static let purple                = Color("Purple")
+  static let ring                  = Color("Ring")
+  static let secondary             = Color("Secondary")
+  static let secondaryForeground   = Color("SecondaryForeground")
+}
